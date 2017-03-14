c058e7763f4cd735a6a78d60decbbaa0d126ccbf
xtian <cristian.ghezzi@gmail.com>
2017-03-14 17:18:21 +0100

    utf8mb4

--- YadaTools/src/main/resources/META-INF/template/script/createDatabaseAndUser.bat
+++ YadaTools/src/main/resources/META-INF/template/script/createDatabaseAndUser.bat
@@ -1,4 +1,4 @@
-mysql.exe --verbose -u root -h localhost -e "create database ${acronym}db$env character set utf8;"
+mysql.exe --verbose -u root -h localhost -e "create database ${acronym}db$env character set utf8mb4;"
 mysql.exe --verbose -u root -h localhost -e "CREATE USER '${acronym}user$env'@'localhost' IDENTIFIED BY '$dbpwd'; GRANT ALL ON ${acronym}db$env.* TO '${acronym}user$env'@'localhost'; FLUSH PRIVILEGES;"
 
 pause
