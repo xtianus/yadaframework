a0e6023b76f1a036f8c0e98cebc1b9bbc7659a6b
Luis Tagle <luis.edu.tagle@hotmail.com>
2017-10-31 16:50:30 +0100

    Add fields modified and version

--- YadaWebSecurity/src/main/java/net/yadaframework/security/persistence/entity/YadaAutoLoginToken.java
+++ YadaWebSecurity/src/main/java/net/yadaframework/security/persistence/entity/YadaAutoLoginToken.java
@@ -2,15 +2,23 @@ package net.yadaframework.security.persistence.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Version;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import net.yadaframework.web.YadaJsonView;
 
 /**
  * Consente di creare un'url da cui accedere direttamente a una pagina con login automatico
@@ -22,6 +30,16 @@ public class YadaAutoLoginToken implements Serializable {
 
 	@SuppressWarnings("unused")
 	private final transient Logger log = LoggerFactory.getLogger(getClass());
+	
+	// For synchronization with external databases
+	@JsonView(YadaJsonView.WithEagerAttributes.class)
+	@Column(columnDefinition="DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date modified;
+	
+	// For optimistic locking
+	@Version
+	protected long version;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -79,6 +97,22 @@ public class YadaAutoLoginToken implements Serializable {
 
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
+	}
+
+	public Date getModified() {
+		return modified;
+	}
+
+	public void setModified(Date modified) {
+		this.modified = modified;
+	}
+
+	public long getVersion() {
+		return version;
+	}
+
+	public void setVersion(long version) {
+		this.version = version;
 	}
 
 	
