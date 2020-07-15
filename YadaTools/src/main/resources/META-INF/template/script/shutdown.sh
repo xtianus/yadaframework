a5a903e7e95601f03aa202a4384c54eccd5b85ab
Cristian Ghezzi <cristian@ghezzi.net>
2020-07-15 17:53:24 +0200

    Latest fixes

--- YadaTools/src/main/resources/META-INF/template/script/shutdown.sh
+++ YadaTools/src/main/resources/META-INF/template/script/shutdown.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script that stops a server and waits until the process is no more active,
 # with a timeout of 10 seconds
+# Note for yada developers: the ${"\$"} in the yada template is the escaped form of the dollar sign
 
 # Command to stop the process
 stopCommand='sudo -u tomcat8 $basePath/$acronym$env/tomcat/bin/shutdown.sh'
@@ -8,16 +9,17 @@ stopCommand='sudo -u tomcat8 $basePath/$acronym$env/tomcat/bin/shutdown.sh'
 # Identification string for the process
 ident='/srv/$acronym$env/tomcat/bin/bootstrap.jar'
 
-myPid=$( ps -ww -C java -o pid,args | grep $ident | awk '{print $1}' )
+myPid=${"\$"}( ps -ww -C java -o pid,args | grep ${"\$"}ident | awk '{print ${"\$"}1}' )
 
-$stopCommand
+touch $basePath/$acronym$env/bin/watchdog.paused
+${"\$"}stopCommand
 
-if [ "$myPid" != "" ]; then
+if [ "${"\$"}myPid" != "" ]; then
 
         COUNTER=0
-        while [  $COUNTER -lt 10 ]; do
-                process=$( ps -h $myPid )
-                if [ "$process" = "" ]; then
+        while [  ${"\$"}COUNTER -lt 10 ]; do
+                process=$( ps -h ${"\$"}myPid )
+                if [ "${"\$"}process" = "" ]; then
                         COUNTER=99;
                 else
                         echo Waiting for process to stop...
@@ -26,10 +28,10 @@ if [ "$myPid" != "" ]; then
                 fi
         done
 
-	if [ "$COUNTER" != "99" ]; then
+	if [ "${"\$"}COUNTER" != "99" ]; then
 		echo "!!!! Can't stop server !!!!"
 		echo "killing process"
-		kill -9 $myPid
+		kill -9 ${"\$"}myPid
 		exit 1
 	fi
 fi
--- YadaTools/src/main/resources/META-INF/template/script/startup.sh
+++ YadaTools/src/main/resources/META-INF/template/script/startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo -u tomcat8 touch $basePath/$acronym$env/logs/restarted
 sudo -u tomcat8 $basePath/$acronym$env/tomcat/bin/startup.sh
 
+rm $basePath/$acronym$env/bin/watchdog.paused 2>/dev/null
