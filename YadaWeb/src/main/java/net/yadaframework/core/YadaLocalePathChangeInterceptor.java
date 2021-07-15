c6994d488177bda3e6aa6424ede2845c38399e19
Cristian Ghezzi <cristian@ghezzi.net>
2021-07-15 16:05:53 +0200

    Small fixes

--- YadaWeb/src/main/java/net/yadaframework/core/YadaLocalePathChangeInterceptor.java
+++ YadaWeb/src/main/java/net/yadaframework/core/YadaLocalePathChangeInterceptor.java
@@ -4,15 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.AsyncHandlerInterceptor;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 /**
  * Locale in the path
  * See https://stackoverflow.com/a/23847484/587641
  */
-public class YadaLocalePathChangeInterceptor extends HandlerInterceptorAdapter {
+public class YadaLocalePathChangeInterceptor implements AsyncHandlerInterceptor {
 	public static final String LOCALE_ATTRIBUTE_NAME = YadaLocalePathChangeInterceptor.class.getName() + ".LOCALE";
 
 	/**
