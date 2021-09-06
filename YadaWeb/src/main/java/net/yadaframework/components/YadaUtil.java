b073ac0302d28ff11e134abe8f7107c1c64c186e
Luis Tagle <luis.edu.tagle@hotmail.com>
2021-09-06 16:30:22 +0200

    Fix image dimensions considering the EXIF "Orientation" flag

--- YadaWeb/src/main/java/net/yadaframework/components/YadaUtil.java
+++ YadaWeb/src/main/java/net/yadaframework/components/YadaUtil.java
@@ -74,6 +74,11 @@ import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+
 import net.yadaframework.core.CloneableDeep;
 import net.yadaframework.core.CloneableFiltered;
 import net.yadaframework.core.YadaConfiguration;
@@ -116,7 +121,7 @@ public class YadaUtil {
 		defaultLocale = config.getDefaultLocale();
 		yadaFileManager = getBean(YadaFileManager.class);
     }
-	
+
 	/**
 	 * Merges all files matched by a pattern, in no particular order.
 	 * @param sourceFolder root folder where files are to be found
@@ -124,7 +129,7 @@ public class YadaUtil {
 	 * @param outputFile file that will contain the joined files
 	 * @param depth (optional) max depth of folders: null or 1 for no recursion
 	 * @param deleteSource (optional) Boolean.TRUE to attempt deletion of source files
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public void joinFiles(Path sourceFolder, String sourceFilePattern, File outputFile, Integer depth, Boolean deleteSource) throws IOException {
 		depth = depth==null ? 1 : depth; // By default we don't look into subfolders
@@ -139,7 +144,7 @@ public class YadaUtil {
 		}
 		joinedStream.close();
 	}
-	
+
 	/**
 	 * Creates a folder in the system temp folder. The name is prefixed with "yada".
 	 * @return
@@ -302,7 +307,11 @@ public class YadaUtil {
 	 * @return dimensions of image, or YadaIntDimension.UNSET when not found
 	 */
 	// Adapted from https://stackoverflow.com/a/12164026/587641
-	public YadaIntDimension getImageDimension(File imageFile) {
+	@Deprecated
+	// Deprecated: the default jpeg image reader does not handle the exif Orientation flag properly
+	// so a "vertical" image with an orientation flag of 6 is considered horizontal
+	// See https://www.impulseadventure.com/photo/exif-orientation.html
+	public YadaIntDimension getImageDimensionLegacy(File imageFile) {
 		String suffix = getFileExtension(imageFile);
 		Iterator<ImageReader> iter = ImageIO.getImageReadersBySuffix(suffix);
 		while (iter.hasNext()) {
@@ -319,6 +328,43 @@ public class YadaUtil {
 			}
 		}
 		return YadaIntDimension.UNSET;
+	}
+
+	/**
+	 * Gets the image dimensions considering the EXIF orientation flag.
+	 * Remember to use the "-auto-orient" flag of the ImageMagick convert command.
+	 * @param imageFile
+	 * @return
+	 */
+	public YadaIntDimension getImageDimension(File imageFile) {
+		try(InputStream stream = new FileInputStream(imageFile))  {
+			Metadata metadata = ImageMetadataReader.readMetadata(stream);
+			//			for (com.drew.metadata.Directory directory2 : metadata.getDirectories()) {
+			//	            for (com.drew.metadata.Tag tag : directory2.getTags()) {
+			//	            	if (tag.getTagName().equalsIgnoreCase("Orientation")) {
+			//	            		System.out.println(tag.getTagName());
+			//	            		System.out.println(tag);
+			//
+			//	            	}
+			//	            }
+			//			}
+			ExifIFD0Directory directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+			int orientation = 1;
+			if (directory.containsTag(ExifIFD0Directory.TAG_ORIENTATION)) {
+				orientation = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+			}
+			ExifSubIFDDirectory directory2 = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+			int width = directory2.getInt(ExifSubIFDDirectory.TAG_EXIF_IMAGE_WIDTH);
+			int height = directory2.getInt(ExifSubIFDDirectory.TAG_EXIF_IMAGE_HEIGHT);
+			if (orientation==6 || orientation==8) {
+				// Image is rotated 90° so dimensions must be swapped
+				return new YadaIntDimension(height, width);
+			}
+			return new YadaIntDimension(width, height);
+		} catch (Exception e) {
+			log.debug("Error reading dimensions for {}", imageFile, e);
+			return YadaIntDimension.UNSET;
+		}
 	}
 
 	/**
