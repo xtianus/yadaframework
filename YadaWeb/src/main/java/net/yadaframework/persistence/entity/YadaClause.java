6605db3e9036e4fef586a0b714e264e5b3bcf1f8
Cristian Ghezzi <cristian@ghezzi.net>
2025-09-30 15:00:55 +0200

    Small fix

--- YadaWeb/src/main/java/net/yadaframework/persistence/entity/YadaClause.java
+++ YadaWeb/src/main/java/net/yadaframework/persistence/entity/YadaClause.java
@@ -38,7 +38,6 @@ public class YadaClause implements Serializable {
 	private int clauseVersion;
 	
 	@Lob
-	@Column(columnDefinition = "longtext") // Needed because of hibernate bug https://hibernate.atlassian.net/browse/HHH-17907
 	private String content;
 
 	public Long getId() {
