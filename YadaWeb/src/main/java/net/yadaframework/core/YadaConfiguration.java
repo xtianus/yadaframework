ef434377db050214165d7cf42561609b9354e75f
Cristian Ghezzi <cristian@ghezzi.net>
2026-03-04 15:02:28 +0100

    session timeout fix

--- YadaWeb/src/main/java/net/yadaframework/core/YadaConfiguration.java
+++ YadaWeb/src/main/java/net/yadaframework/core/YadaConfiguration.java
@@ -110,6 +110,7 @@ public abstract class YadaConfiguration {
 	private File tempFolder = null;
 	private String googleApiKey = null;
 	private Integer bootstrapVersion = null;
+	private Integer sessionTimeoutMinutes = null;
 	
 	/**
 	 * Copy the already initialised configuration to a different instance
@@ -120,6 +121,19 @@ public abstract class YadaConfiguration {
 		yadaConfiguration.configuration = this.configuration;
 	}
 
+	public Integer getSessionTimeoutMinutes() {
+		if (sessionTimeoutMinutes==null) {
+			sessionTimeoutMinutes = configuration.getInt("config/security/sessionTimeoutMinutes", -1);
+			if (sessionTimeoutMinutes==-1) {
+				// When using standalone tomcat server the session timeout must be configured with the same value set in web.xml
+				// otherwise the session might never expire because the javascript code would keep it awake.
+				log.error("Session timeout not configured correctly (defaulting to 20). Please configure session timeout in conf.webapp.xml equal to web.xml");
+				sessionTimeoutMinutes = 20;
+			}
+		}
+		return sessionTimeoutMinutes;
+	}
+	
 	/**
 	 * Returns the configured timeout for asynchronous requests in seconds.
 	 * Equivalent to the Tomcat parameter asyncTimeout, but more effective (the application-defined parameter takes precedence).
